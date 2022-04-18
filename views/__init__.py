896a8720e05d340ba76bc0c0d8c5b9e3c04425b0
Henry House <hankhaus99@gmail.com>
2022-04-18 14:40:25 -0500

    chapter 16 update working

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,3 @@
 from .entry_requests import get_all_entries, get_single_entry, create_journal_entry
-from .entry_requests import delete_entry, get_entries_by_search
+from .entry_requests import delete_entry, get_entries_by_search, update_entry
 from .mood_requests import get_all_moods
